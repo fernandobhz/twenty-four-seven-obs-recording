e4c9f4bde96b37977f3a17b7063b5f6353e8ae79
Fernando Reis Guimarães <fernandobhz@gmail.com>
2023-03-06 23:37:11 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Twenty Four Seven Obs Recording
+
+This is an small app that will start obs in recording mode and will restart it every hour.
+
+# Usage
+
+Just put the TwentyFourSevenObsRecording.exe in the same folder of obs64.exe and execute it.
+
+# How it works
+
+It won't produce any single message ou interation.  
+It will just start the Obs and restart it every hour.
+Please let the Obs already configured to record what you want.
---
+++ TwentyFourSevenObsRecording/Module1.vb
@@ -0,0 +1,38 @@
+﻿Imports System.IO
+Imports System.Diagnostics
+Imports System.Timers
+
+Module Module1
+    Const RECORDING_TIME_BOX = 3600 * 1000
+    Const OVERLAP_PERIOD = 60 * 1000
+
+    Sub startProcess(Timeout As Long)
+        Dim Process As New Process()
+        Process.StartInfo.FileName = "obs64.exe"
+        Process.StartInfo.Arguments = "--startrecording -m"
+        Process.Start()
+
+        System.Threading.Thread.Sleep(Timeout)
+        Process.Kill()
+    End Sub
+
+    Function MillisecondsToNextHour() As Long
+        Dim CurrentTime As DateTime = DateTime.Now
+        Dim NextHour As DateTime = CurrentTime.AddHours(1).Date.AddHours(CurrentTime.Hour + 1)
+        Dim Timeout As Long = (NextHour - CurrentTime).TotalMilliseconds
+        Return Timeout
+    End Function
+
+    Sub Main(args As String())
+        Dim Timeout As Long = MillisecondsToNextHour()
+        'Timeout = RECORDING_TIME_BOX 'For testing in development I need shorter period
+
+        Do
+            Dim Thread As New System.Threading.Thread(Sub() startProcess(Timeout))
+            Thread.Start()
+
+            System.Threading.Thread.Sleep(Timeout - OVERLAP_PERIOD)
+        Loop
+
+    End Sub
+End Module
